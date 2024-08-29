398ef1aba49e93694b8904ac1afbceb0946daaf8
Pedro Perez <plperezpiris@gmail.com>
2024-08-29 23:09:02 +0200

    almost done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,40 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+    console.log(num1)
+    console.log(num2)
+if(num1 > num2){
+    return num1
+}else if (num1 <num2) {
+    return num2
+    }else{
+        return "Both numbers are the same"
+    }
+}
+console.log(maxOfTwoNumbers(9, 3))
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr){
+
+    if(arr.length === 0 ){
+        return null
+    }
+    let result2 = []
+    for (let i =0 ; i< arr.length; i++){
+      
+        if(arr[i].length >result2.length){
+            result2 = arr[i]
+        }
+    }
+    return result2
+}
+console.log(findLongestWord(words))
+
+ 
+    
 
 
 
@@ -15,20 +42,58 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
 
-
+function sumNumbers(arr){
+    let result = 0
+if(arr === []){
+    return null
+}
+else{
+    for(let i = 0; i<arr.length; i++){
+    
+        result += arr[i]
+    }
+    
+}
+return result
+console.log(sumNumbers(numbers))
+}
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+console.log(sumNumbers(numbers2))
 
+function averageNumbers(arr){
+let result = 0
+if (arr === []){
+    return null
+}else{
+for (i = 0; i < arr.length; i++){
+        result += arr[i] /arr.length
+    }
+}
 
+return result
 
+}
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr) {
+    if(arr.length===0){
+        return null
+}else{
+    for(let i= 0; i<arr.length; i++){
+        if(arr.includes(arr[i])){
+            return true
+        }else{
+            return false
+        }
+    }
+
+}
+return arr
+}
